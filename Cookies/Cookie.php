a472f0cc94db78025c0bc60dd6d309be005c54eb
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-04-19 20:37:27 +0200

    exercises: change some static methods of the cookie class

--- Cookies/Cookie.php
+++ Cookies/Cookie.php
@@ -5,43 +5,25 @@ class Cookie
     public $name;
     public $value;
     public $exdays;
-    public $userLogin = 'user';
+
 
     public function __construct($name, $value, $exdays = 1)
     {
         $this->name = $name;
         $this->value = $value;
         $this->exdays = $exdays;
-        setcookie($this->name, $this->value, $this->exdays);
-    }
-    public static function create($value)
-    {
-        setcookie($this->userLogin, $value, time() + 3600);
+
+        setcookie($this->name, $this->value, time() + 3600);
     }
 
-    public static function delete()
+    public static function delete($name)
     {
-        setcookie($this->userLogin, "", time() - 3600);
+        setcookie($name, "", time() - 3600);
     }
 
-    public static function islogin($users, $userName, $password)
+    public static function isExists($name)
     {
-        for ($i = 0; $i < count($users); $i++) {
-            if ($userName == $users[$i]['Usuario'] && $password == $users[$i]['Clave']) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public static function isExists($userName)
-    {
-//    if (!isset($_COOKIE[$userName])) {
-        //        echo "Cookie named '" . $userName . "' is not set!";
-        //    } else {
-        //        echo "Cookie '" . $userName . "' is set!<br>";
-        //    }
-        return isset($_COOKIE[$userName]);
+        return isset($_COOKIE[$name]);
     }
 
     public function getName()

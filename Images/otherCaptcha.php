8ce564de612438495adc7f226a8d136be15d0572
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-04-28 02:33:16 +0200

    exercises: try solution

--- Images/otherCaptcha.php
+++ Images/otherCaptcha.php
@@ -12,47 +12,51 @@ error_reporting(E_ALL);
         <td>A</td>
         <td>B</td>
         <td>C</td>
-        <td>D</td>
-        <td>E</td>
+        <!-- <td>D</td>
+        <td>E</td> -->
     </tr>
     <?php
-$letter = "ABCDE";
-$row = "";
-$col = "";
+    
+    $letter = "ABC";
+    $rowNumber = "";
+    $columLetter = "";
+    $captcha = "";
 
-for ($i = 0; $i < 5; $i++): ?>
+    for ($i = 0; $i < 3; $i++): ?>
         <tr>
             <td> <?=$i?></td>
             <?php
-                $row .= rand(0, 4);
-                $col .= $letter[rand(0, strlen($letter) - 1)];
+                $rowNumber .= rand(0, 2);            
+                $columLetter .= $letter[rand(0, strlen($letter) - 1)];
 
-                for ($j = 0; $j < 5; $j++):
+                for ($j = 0; $j < 3; $j++):
                     $image[$i] = new Image(100, 75);
-                    $image[$i]->fill($image[$i]->getColorRandom());
+                    // $image[$i]->fill($image[$i]->getColorRandom());
+                    $image[$i]->fill($image[$i]->getColorBlack());
                     $letters = $image[$i]->generateLettersRandom(1);
 
-                    $image[$i]->writeTextInImage($letters, 20, 35, $image[$i]->getColorRandom(), -40);
-                    $image[$i]->paintLineRandom(5, 100, 75, $image[$i]->getColorRandom());
+                    // $image[$i]->writeTextInImage($letters, 20, 35, $image[$i]->getColorRandom(), -40);
+                    $image[$i]->writeTextInImage($letters, 20, 35, $image[$i]->getColorWhite(), -40);
+                    // $image[$i]->paintLineRandom(5, 100, 75, $image[$i]->getColorRandom());
+                    
+                    for ($k = 0; $k < 5; $k++) {
+                        if ($rowNumber[$i] == $k) {
+                            if ($columLetter[$i] == $letter[$j]) {
+                                echo $letters;
+                                $captcha .=  $letters;
+                            }
+                        }
+                        
+                    }
             ?>
-
-	            <td><img src="<?=$image[$i]->getSrc();?>" alt=""></td>
-	            <?php endfor;?>
+	        <td><img src="<?=$image[$i]->getSrc();?>" alt=""></td>
+	        <?php endfor;?>
         </tr>
     <?php endfor;?>
 </table>
 
-<?php for ($k = 0; $k < 5; $k++):
-    if ($col[$k] == $letter[$j]) {
-        //     if ($row == $i) {
-        //         echo $letters;
-        //         $captcha =  $letters;
-        //     }
-    }
-endfor?>
-
 <form action="" method="post">
-    <label for="resultCaptcha"><?=$row . $col?>
+    <label for="resultCaptcha"><?=$rowNumber . "<br>" . $columLetter?>
         <input type="text" name="resultCaptcha" id="resultCaptcha">
     </label>
     <input type="hidden" name=letters value="<?=$captcha?>">
@@ -64,7 +68,6 @@ endfor?>
 if (!isset($_POST['send'])) {
     return;
 }
-
 if ($_POST['letters'] == $_POST['resultCaptcha']) {
     echo "Coincide";
 } else {

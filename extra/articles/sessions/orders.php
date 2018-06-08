0e2bd8ecdb84802460d06e13ba9e2180f477cde0
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-06-08 23:12:53 +0200

    exercises: remove articles to basket

--- extra/articles/sessions/orders.php
+++ extra/articles/sessions/orders.php
@@ -12,15 +12,19 @@ echo "<h2>Productos seleccionados</h2>";
 
 $db = new DataBasePDO();
 $db->setTable('articulos');
-$articles = array_keys($_POST['arr']);
 
+if (isset($_POST['arr'])) {
+    $articles = array_keys($_POST['arr']);
+} else {
+    $articles = [];
+}
 foreach ($articles as $key => $article) {
     if (!isset($_SESSION['basket']) || !in_array($article, $_SESSION['basket'])) {
         $_SESSION['basket'][$articles[$key]] = $article;
     }
 }
 ?>
-<table border="2px"> 
+<table border="2px">
     <tr>
         <td>Id</td>
         <td>Nombre</td>
@@ -30,34 +34,50 @@ foreach ($articles as $key => $article) {
         <td>Familia</td>
         <td>Imagen</td>
         <td>Tipo</td>
+        <td>Cantidad</td>
+        <td>Eliminar</td>
     </tr>
-    
-    <?php
-    $sum = 0;
-    
-    foreach ($_SESSION['basket'] as $ar) {
+
+    <form action="orders.php" method="POST">
+        <?php
+            $sum = 0;
+            foreach ($_SESSION['basket'] as $ar) {
         ?>
         <tr>
-            <?php
-            $article = $db->read('Id', $ar);
-            $string = base64_decode($article[0]['Imagen']);
-            $image = Image::createImageFromString($string);
+            <?php                    
+                $article = $db->read('Id', $ar);
+                $string = base64_decode($article[0]['Imagen']);
+                $image = Image::createImageFromString($string);
             ?>
-            <td><?= $article[0]['Id'] ?></td>
-            <td><?= $article[0]['Nombre'] ?></td>
-            <td><?= $article[0]['Marca'] ?></td>
-            <td><?= $article[0]['Modelo'] ?></td>
-            <td><?= $article[0]['Precio'] ?></td>
-            <td><?= $article[0]['Familia'] ?></td>
+            <td><?=$article[0]['Id']?></td>
+            <td><?=$article[0]['Nombre']?></td>
+            <td><?=$article[0]['Marca']?></td>
+            <td><?=$article[0]['Modelo']?></td>
+            <td><?=$article[0]['Precio']?></td>
+            <td><?=$article[0]['Familia']?></td>
             <td><img src='<?=$image->getSrc();?>' style="width: 100px; hight: 100px;"></td>
-            <td><?= $article[0]['Tipo'] ?></td>
+            <td><?=$article[0]['Tipo']?></td>
+            <td><input type="number" name="amount[<?=$article[0]['Id']?>]" value=""></td>
+            <td><input type=checkbox name=remove[<?=$article[0]['Id']?>] value=<?=$article[0]['Id']?>></td>
         </tr>
         <?php
-        $sum += $article[0]['Precio'];
+            $sum += $article[0]['Precio'];
+        }
+        ?>
+        <tr>
+            <td colspan="6">PRECIO TOTAL</td>
+            <td colspan="2"><?=$sum?></td>
+        </tr>
+</table>
+        <input type="submit" value="Eliminar" name="delete">
+    </form>
+
+<?php
+if (!isset($_POST['delete'])) return;
+
+if (isset($_POST['remove'])) {
+    $remove = $_POST['remove'];
+    foreach ($remove as $key => $value) {
+        unset($_SESSION['basket'][$key]);
     }
-    ?>
-    <tr>
-        <td colspan="6">PRECIO TOTAL</td>
-        <td colspan="2"><?= $sum ?></td>
-    </tr>
-</table>  
+}

468712baa445e8670a5bf4fb0abd42c14baa6612
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-06-12 19:04:28 +0200

    exercises: Change library to connect data base

--- extra/basket/index.php
+++ extra/basket/index.php
@@ -1,82 +1,108 @@
 
-<form name=f1 method="post" action=<?php echo $_SERVER['PHP_SELF'] ?>>
+<?php
+include dirname(__FILE__) . '/../../DataBase/DataBasePDO.php';
+
+$db = new DataBasePDO();
+$db->setTable('familia');
+$families = $db->readAll();
+?>
+
+<form action="" method="POST">
     <div>
-        <label for="familia">Familia</label>
-        <select name=familia><br>
+        <label for="family">Familia</label>
+        <select name="family"><br>
             <option value=""></option>
-            <?php
-                require_once "libreria.php";
-                $db = conectaDb();
-                $consulta = "select * from familia";
-                $filas = ConsultaSelect($db, $consulta);
-                $family = "";
-                if (isset($_POST['familias'])) {
-                    $family = $_POST['familias'];
-                }
-                foreach ($filas as $fila) {
-                    $linea = "<option value=$fila[cod] ";
-                    if ($fila['cod'] == $family) {
-                        $linea .= " selected ";
-                    }
-                    $linea .= " >$fila[nombre]</option>";
-                    echo $linea;
-                }
-                cerrarDb($db);
-                ?>
+                <?php foreach($families as $family): ?>
+                <option value="<?= $family['cod']; ?>"
+                   <?php if(isset($_POST['family']) && $_POST['family'] == $family['cod']) echo 'selected';?>>
+                   <?= $family['nombre']?>
+                </option>
+                <?php endforeach;?>
         </select>
     </div>
     <div>
-        <label for="nombre">Nombre Producto</label>
-        <input type=text name=nombre value=<?php if (isset($_POST['nombre'])) echo $_POST['nombre']; ?>>
+        <label for="name">Nombre Producto</label>
+        <input type=text name="name" value=<?php if (isset($_POST['nombre'])) echo $_POST['nombre']; ?>>
     </div>
     <div>
-        <input type=submit name=buscar value=buscar>
+        <input type=submit name="search" value=buscar>
     </div>
 </form>
-<?php
-if (!isset($_POST['buscar'])) {
-    return;
-}
 
-$family = $_POST['familia'];
-$name = $_POST['nombre'];
-$consulta = "SELECT p.cod,p.nombre_corto,p.PVP,b.suma
-            FROM  ( SELECT s.producto,sum(s.unidades) AS suma FROM stock s GROUP BY 1) b,producto p
-            WHERE p.cod=b.producto ";
-$param = array();
+<?php
+if (!isset($_POST['search'])) return;
+
+$family = $_POST['family'];
+$name = $_POST['name'];
+$query = "SELECT p.cod,p.nombre_corto,p.PVP,b.suma FROM  
+        (SELECT s.producto,sum(s.unidades) AS suma FROM stock s GROUP BY 1) 
+        b,producto p WHERE p.cod=b.producto ";
+
+$params = [];
 if (!empty($family)) {
-    $consulta .= " and familia=:familia ";
-    $param[":familia"] = $family;
+    $query .= " and familia=:familia ";
+    $params[":familia"] = $family;
 }
 if (!empty($name)) {
-    $consulta .= " and nombre_corto like :nombre_corto";
-    $param[":nombre_corto"] = "%" . $name . "%";
+    $query .= " and nombre_corto like :nombre_corto";
+    $params[":nombre_corto"] = "%" . $name . "%";
 }
-$filas = ConsultaSelectSeg($db, $consulta, $param);
+$rows = $db->query($query, $params);
 
-echo "<form name=f2 action=pedir.php method=post>";
-echo "Clientes <select name=cliente>";
-$consulta = "select nif, nombre, apellido1, apellido2 from clientes";
-$filas2 = consultaSelect($db, $consulta);
-foreach ($filas2 as $fila) {
-    echo "<option value=$fila[nif]>$fila[apellido1] $fila[apellido2], $fila[nombre]</option>";
-}
-echo "</select><br><br>";
-
+$db->setTable('clientes');
+$clients = $db->readAll();
 $cont = 0;
-echo "<table border=2>";
-echo "<tr><td>Seleccionar</td><td>Nombre</td><td>Precio</td><td>Stock</td><td>Cantidad</td></tr>";
-foreach ($filas as $fila) {
-    echo "<tr>";
-    echo "<td><input type=checkbox name=pro[$cont] value=$fila[cod]></td>
-    <td>" . $fila['nombre_corto'] . "</td><td>" . $fila['PVP'] . "</td>
-    <td>" . $fila['suma'] . "</td>
-    <td><input type=text size=4 name=cant[$cont]></td>
-    <input type=hidden name=stock[$cont] value=$fila[suma]>
-    <input type=hidden name=pvp[$cont] value=$fila[PVP]>";
-    echo "</tr>";
-    $cont++;
-}
-echo "</table>";
-echo "<input type=submit value=pedir>";
-echo "</form>";
+?>
+<form name=f2 action=pedir.php method=post>
+    <div>
+        <label for="client">Cliente</label>
+        <select name="client" id="client">
+            <option value=""></option>
+            <?php foreach($clients as $client): ?>
+                <option value="<?= $client['NIF']; ?>"
+                    <?php if(isset($_POST['client']) && $_POST['client'] == $client['NIF']) echo 'selected';?>>
+                    <?= $client['Apellido1'] . " " .  $client['Apellido2'] . " " . $client['Nombre']?>
+                </option>
+            <?php endforeach;?>
+        </select>
+    </div>
+    <table border="2px">
+        <tr>
+            <td>Seleccionar</td>
+            <td>Nombre</td>
+            <td>Precio</td>
+            <td>Stock</td>
+            <td>Cantidad</td>
+        </tr>
+        <?php foreach($rows as $row): ?>
+            <tr>
+                <td><input type=checkbox name=pro[$cont] value=$fila[cod]></td>
+                <td><?= $row['nombre_corto'] ?></td>
+                <td><?= $row['PVP'] ?></td>
+                <!-- <td><?= $fila['suma'] ?></td> -->
+                <td><input type="text" size="4" name="cant[$cont]"></td>
+                <input type=hidden name=stock[$cont] value=$fila[suma]>
+                <input type=hidden name=pvp[$cont] value=$fila[PVP]>";
+            </tr>
+        <?php endforeach; ?>
+    </table>
+    <input type="submit" value="pedir">
+</form>
+<?php
+// $cont = 0;
+// echo "<table border=2>";
+// echo "<tr><td>Seleccionar</td><td>Nombre</td><td>Precio</td><td>Stock</td><td>Cantidad</td></tr>";
+// foreach ($rows as $fila) {
+//     echo "<tr>";
+//     echo "<td><input type=checkbox name=pro[$cont] value=$fila[cod]></td>
+//     <td>" . $fila['nombre_corto'] . "</td><td>" . $fila['PVP'] . "</td>
+//     <td>" . $fila['suma'] . "</td>
+//     <td><input type=text size=4 name=cant[$cont]></td>
+//     <input type=hidden name=stock[$cont] value=$fila[suma]>
+//     <input type=hidden name=pvp[$cont] value=$fila[PVP]>";
+//     echo "</tr>";
+//     $cont++;
+// }
+// echo "</table>";
+// echo "<input type=submit value=pedir>";
+// echo "</form>";

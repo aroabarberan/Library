cbfa1634b35b297e1e9b98dbac0824d8676196cb
Aroa Barberan Poblete <aroabarberan@gmail.com>
2018-06-13 01:05:05 +0200

    exercises: fix content function of class Process

--- extra/basket/Process.php
+++ extra/basket/Process.php
@@ -1,49 +1,67 @@
 <?php
+// include dirname(__FILE__) . '/../../DataBase/DataBasePDO.php';
 
 class Process
 {
 
     public static function nextStore($stores, $cod)
     {
-        // $pos = array_search($cod, array_keys($stores));
-        // if ($pos == count($stores) - 1) {
-        //     reset($stores);
-        // }
-        // return each($stores);
+        $pos = array_search($cod, array_keys($stores));
+        if ($pos == count($stores) - 1) {
+            reset($stores);
+        }
+        return each($stores);
     }
 
     public static function updateStore(&$stores, $cod)
     {
-        // $stores[$cod] = $stores[$cod] - 1;
-        // if ($stores[$cod] == 0) {
-        //     unset($stores[$cod]);
-        //     $consulta = "delete from stock where unidades=0";
-        //     consultaSimple($db, $consulta);
-        // }
+        $db = new DataBasePDO();
+        $stores[$cod] = $stores[$cod] - 1;
+        if ($stores[$cod] == 0) {
+            unset($stores[$cod]);
+            $query = "DELETE FROM stock WHERE unidades=0";
+            $db->query($query, $params);
+        }
     }
     public static function getStore($cod)
     {
-        // $tiendas = array();
-        // $consulta = "select tienda from stock where producto=$cod";
-        // $filas = consultaSelect($db, $consulta);
-        // foreach ($filas as $fila) {
-        //     $tiendas[$fila['tienda']] = $fila['unidades'];
-        // }
-        // return $tiendas;
+        $db = new DataBasePDO();
+        $store = [];
+        $rows = $db->query("SELECT tienda FROM stock WHERE producto='$cod'");
+        foreach ($rows as $row) {
+            $store[$row['tienda']] = $row['unidades'];
+        }
+        return $store;
     }
 
     public static function reduceStock($cod, $amount)
     {
-        // $tiendas = sacarTiendas($db, $cod);
-        // list($tien, $unidades) = each($tiendas); //al sacar en un array primero se comprobara en la central, si no hay suficiente en la tienda 1 y por ultimo en la tienda 2
-        // while ($amount > 0) {
-        //     $consulta = "update stock set unidades = (unidades-1) where producto=:producto and tienda=:tienda";
-        //     $param = array(":producto" => $cod, ":tienda" => $tien);
-        //     consultaSimpleSeg($db, $consulta, $param);
-        //     actualizaTiendas($db, $tiendas, $tien); //Actualizamos el array
-        //     $amount--;
-        //     list($tien, $unidades) = siguienteTienda($tiendas, $tien);
+        $db = new DataBasePDO();
+        $store = getStore($cod);
+        list($tien, $unidades) = each($store);
+        while ($amount > 0) {
+            $query = "UPDATE stock SET unidades = (unidades-1) WHERE producto=:producto AND tienda=:tienda";
+            $params= [":producto" => $cod, ":tienda" => $tien];
+            $db->query($query, $params);
+            updateStore($store, $tien);
+            $amount--;
+            list($tien, $unidades) = nextStore($store, $tien);
 
-        // }
+        }
     }
+    public static function showError($message)
+    {
+        echo "<font color=red size=5>$message</font>";
+        echo "<a href=index.php>Repetir Pedido</a>";
+    }
+
+    public static function amountAvailable($stock, $amounts)
+    {
+        $i = 0;
+        while (($i < count($stock)) && ($stock[$i] >= $amounts[$i])) {
+            $i++;
+        }
+        return (count($stock) == $i);
+    }
+
 }
